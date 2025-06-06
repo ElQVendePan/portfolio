cffa867f63efe75b880c0e8de7fc91ba75e7e8c8
ElQVendePan <danielmenses282@gmail.com>
2025-06-06 01:56:21 -0500

    update

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -2,10 +2,9 @@ import React from 'react'
 
 type ButtonProps = {
     children: React.ReactNode;
-    color?: string;
 };
 
-const Button = ({ children, color }: ButtonProps) => {
+const Button = ({ children }: ButtonProps) => {
     return (
         <button className={`p-2 px-5 rounded-full bg-purple-50 ring-2 ring-purple-600 text-purple-600`}>{children}</button>
     )
